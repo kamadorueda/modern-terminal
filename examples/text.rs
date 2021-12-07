d0a0e689b8cf2b963e6c25165f277004e90a651b
Kevin Amado <kamadorueda@gmail.com>
2021-12-07 17:12:10 -0500

    refactor: nicer text example

--- examples/text.rs
+++ examples/text.rs
@@ -12,27 +12,18 @@ fn main() -> std::io::Result<()> {
     console.render(&Text {
         align:    TextAlignment::Left,
         portions: vec![
-            SegmentPortion::Style(Style::Background("bright_blue".to_string())),
+            SegmentPortion::Style(Style::Bold),
+            SegmentPortion::Style(Style::Foreground("yellow".to_string())),
+            SegmentPortion::Style(Style::Underline),
+            SegmentPortion::Text("TextAlignment::Left".to_string()),
+        ],
+    })?;
+    console.render(&Text {
+        align:    TextAlignment::Left,
+        portions: vec![
+            SegmentPortion::Style(Style::Background("cyan".to_string())),
             SegmentPortion::Style(Style::Foreground("black".to_string())),
-            SegmentPortion::Text("Background bright_blue".to_string()),
-        ],
-    })?;
-    console.render(&Text {
-        align:    TextAlignment::Left,
-        portions: vec![
-            SegmentPortion::Style(Style::Blink),
-            SegmentPortion::Text("Blink".to_string()),
-            SegmentPortion::Style(Style::None),
-            SegmentPortion::Text("¹".to_string()),
-        ],
-    })?;
-    console.render(&Text {
-        align:    TextAlignment::Left,
-        portions: vec![
-            SegmentPortion::Style(Style::BlinkFast),
-            SegmentPortion::Text("BlinkFast".to_string()),
-            SegmentPortion::Style(Style::None),
-            SegmentPortion::Text("¹".to_string()),
+            SegmentPortion::Text("Background cyan".to_string()),
         ],
     })?;
     console.render(&Text {
@@ -45,84 +36,52 @@ fn main() -> std::io::Result<()> {
     console.render(&Text {
         align:    TextAlignment::Left,
         portions: vec![
-            SegmentPortion::Text("Conceal".to_string()),
             SegmentPortion::Style(Style::None),
-            SegmentPortion::Text("¹".to_string()),
+            SegmentPortion::Text("None".to_string()),
         ],
     })?;
-    console.render(&Text {
-        align:    TextAlignment::Left,
-        portions: vec![
-            SegmentPortion::Style(Style::Dim),
-            SegmentPortion::Text("Dim".to_string()),
-            SegmentPortion::Style(Style::None),
-            SegmentPortion::Text("¹".to_string()),
-        ],
-    })?;
+
     console.render(&Text {
         align:    TextAlignment::Center,
         portions: vec![
-            SegmentPortion::Style(Style::Encircle),
-            SegmentPortion::Text("Encircle".to_string()),
-            SegmentPortion::Style(Style::None),
-            SegmentPortion::Text("¹".to_string()),
-        ],
-    })?;
-    console.render(&Text {
-        align:    TextAlignment::Center,
-        portions: vec![
-            SegmentPortion::Style(Style::Frame),
-            SegmentPortion::Text("Frame".to_string()),
-            SegmentPortion::Style(Style::None),
-            SegmentPortion::Text("¹".to_string()),
-        ],
-    })?;
-    console.render(&Text {
-        align:    TextAlignment::Center,
-        portions: vec![
+            SegmentPortion::Style(Style::Bold),
             SegmentPortion::Style(Style::Foreground("yellow".to_string())),
-            SegmentPortion::Text("Foreground yellow".to_string()),
+            SegmentPortion::Style(Style::Underline),
+            SegmentPortion::Text("TextAlignment::Center".to_string()),
         ],
     })?;
     console.render(&Text {
         align:    TextAlignment::Center,
         portions: vec![
-            SegmentPortion::Style(Style::Foreground("#FFFF00".to_string())),
-            SegmentPortion::Text("Foreground #FFFF00".to_string()),
+            SegmentPortion::Style(Style::Bold),
+            SegmentPortion::Style(Style::Foreground("cyan".to_string())),
+            SegmentPortion::Text("Foreground cyan".to_string()),
+        ],
+    })?;
+    console.render(&Text {
+        align:    TextAlignment::Center,
+        portions: vec![
+            SegmentPortion::Style(Style::Foreground("#00FFFF".to_string())),
+            SegmentPortion::Text("Foreground #00FFFFF".to_string()),
         ],
     })?;
     console.render(&Text {
         align:    TextAlignment::Center,
         portions: vec![
             SegmentPortion::Style(Style::Foreground(
-                "rgb(255,255,0)".to_string(),
+                "rgb(0,255,255)".to_string(),
             )),
-            SegmentPortion::Text("Foreground rgb(255,255,0)".to_string()),
+            SegmentPortion::Text("Foreground rgb(0,255,255)".to_string()),
         ],
     })?;
-    console.render(&Text {
-        align:    TextAlignment::Center,
-        portions: vec![
-            SegmentPortion::Style(Style::Italic),
-            SegmentPortion::Text("Italic".to_string()),
-            SegmentPortion::Style(Style::None),
-            SegmentPortion::Text("¹".to_string()),
-        ],
-    })?;
-    console.render(&Text {
-        align:    TextAlignment::Center,
-        portions: vec![
-            SegmentPortion::Style(Style::None),
-            SegmentPortion::Text("None".to_string()),
-        ],
-    })?;
+
     console.render(&Text {
         align:    TextAlignment::Right,
         portions: vec![
-            SegmentPortion::Style(Style::Overline),
-            SegmentPortion::Text("Overline".to_string()),
-            SegmentPortion::Style(Style::None),
-            SegmentPortion::Text("¹".to_string()),
+            SegmentPortion::Style(Style::Bold),
+            SegmentPortion::Style(Style::Foreground("yellow".to_string())),
+            SegmentPortion::Style(Style::Underline),
+            SegmentPortion::Text("TextAlignment::Right".to_string()),
         ],
     })?;
     console.render(&Text {
@@ -137,40 +96,66 @@ fn main() -> std::io::Result<()> {
     console.render(&Text {
         align:    TextAlignment::Right,
         portions: vec![
-            SegmentPortion::Style(Style::Strike),
-            SegmentPortion::Text("Strike".to_string()),
-            SegmentPortion::Style(Style::None),
-            SegmentPortion::Text("¹".to_string()),
-        ],
-    })?;
-    console.render(&Text {
-        align:    TextAlignment::Right,
-        portions: vec![
             SegmentPortion::Style(Style::Underline),
             SegmentPortion::Text("Underline".to_string()),
             SegmentPortion::Style(Style::None),
             SegmentPortion::Text(" ".to_string()),
         ],
     })?;
+
     console.render(&Text {
-        align:    TextAlignment::Right,
+        align:    TextAlignment::Center,
         portions: vec![
-            SegmentPortion::Style(Style::UnderlineMore),
-            SegmentPortion::Text("UnderlineMore".to_string()),
-            SegmentPortion::Style(Style::None),
-            SegmentPortion::Text("¹".to_string()),
+            SegmentPortion::Style(Style::Bold),
+            SegmentPortion::Style(Style::Foreground("yellow".to_string())),
+            SegmentPortion::Style(Style::Underline),
+            SegmentPortion::Text("Supported in some terminals".to_string()),
         ],
     })?;
     console.render(&Text {
-        align:    TextAlignment::Left,
+        align:    TextAlignment::Center,
         portions: vec![
-            SegmentPortion::Style(Style::Bold),
-            SegmentPortion::Style(Style::Foreground(
-                "bright_green".to_string(),
-            )),
-            SegmentPortion::Text(
-                "¹ Only supported in some terminals".to_string(),
-            ),
+            SegmentPortion::Style(Style::Blink),
+            SegmentPortion::Text("Blink".to_string()),
+            SegmentPortion::Style(Style::None),
+            SegmentPortion::Text(" ".to_string()),
+            SegmentPortion::Style(Style::BlinkFast),
+            SegmentPortion::Text("BlinkFast".to_string()),
+            SegmentPortion::Style(Style::None),
+            SegmentPortion::Text(" ".to_string()),
+            SegmentPortion::Style(Style::Conceal),
+            SegmentPortion::Text("Conceal".to_string()),
+            SegmentPortion::Style(Style::None),
+            SegmentPortion::Text(" ".to_string()),
+            SegmentPortion::Style(Style::Dim),
+            SegmentPortion::Text("Dim".to_string()),
+            SegmentPortion::Style(Style::None),
+            SegmentPortion::Text(" ".to_string()),
+            SegmentPortion::Style(Style::Encircle),
+            SegmentPortion::Text("Encircle".to_string()),
+        ],
+    })?;
+    console.render(&Text {
+        align:    TextAlignment::Center,
+        portions: vec![
+            SegmentPortion::Style(Style::Frame),
+            SegmentPortion::Text("Frame".to_string()),
+            SegmentPortion::Style(Style::None),
+            SegmentPortion::Text(" ".to_string()),
+            SegmentPortion::Style(Style::Italic),
+            SegmentPortion::Text("Italic".to_string()),
+            SegmentPortion::Style(Style::None),
+            SegmentPortion::Text(" ".to_string()),
+            SegmentPortion::Style(Style::Overline),
+            SegmentPortion::Text("Overline".to_string()),
+            SegmentPortion::Style(Style::None),
+            SegmentPortion::Text(" ".to_string()),
+            SegmentPortion::Style(Style::Strike),
+            SegmentPortion::Text("Strike".to_string()),
+            SegmentPortion::Style(Style::None),
+            SegmentPortion::Text(" ".to_string()),
+            SegmentPortion::Style(Style::UnderlineMore),
+            SegmentPortion::Text("UnderlineMore".to_string()),
         ],
     })?;
 
